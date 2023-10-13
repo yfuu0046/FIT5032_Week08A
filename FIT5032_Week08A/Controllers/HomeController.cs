4472307a61fb7c034b404a12f3d8dfb648202483
Yixiao Fu <yfuu0046@student.monash.edu>
2023-10-13 22:30:58 +1100

    Add project files.

---
+++ FIT5032_Week08A/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using FIT5032_Week08A.Models;
+using FIT5032_Week08A.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_Week08A.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Please comment out these codes once you have registered your API key.
+            //EmailSender es = new EmailSender();
+            //es.RegisterAPIKey();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        public ActionResult LoadDynamicContent()
+        {
+            // 這裡可以返回動態載入的內容，可以是一段 HTML 代碼或者一個部分視圖
+            return PartialView("_DynamicContent");
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model)
+        {
+            try
+            {
+                if (model == null)
+                {
+                    throw new ArgumentNullException(nameof(model), "The 'model' parameter cannot be null.");
+                }
+
+                if (model.Attachment == null)
+                {
+                    throw new ArgumentNullException(nameof(model.Attachment), "No attachment provided.");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+                    HttpPostedFileBase attachment = model.Attachment;
+
+                    EmailSender es = new EmailSender();
+                    es.Send(toEmail, subject, contents, attachment);
+
+                    ViewBag.Result = "Email has been sent.";
+                    ModelState.Clear();
+                    return View(new SendEmailViewModel());
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Result = "An error occurred while sending the email. Error message: " + ex.Message;
+            }
+
+            return View();
+        }
+
+
+    }
+
+}
+
+
+
+
+    
---
+++ FIT5032_Week08A/Utils/EmailSender.cs
@@ -0,0 +1,43 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+        // Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public async Task Send(String toEmail, String subject, String contents, HttpPostedFileBase postedFile)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("arvinfu99@outlook.com", "FIT5032 Example Email User");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            if (postedFile != null)
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    postedFile.InputStream.CopyTo(memoryStream);
+                    byte[] bytes = memoryStream.ToArray();
+                    Attachment attachment = new Attachment();
+                    attachment.Content = Convert.ToBase64String(bytes);
+                    attachment.Filename = postedFile.FileName;
+                    msg.AddAttachment(attachment.Filename, attachment.Content);
+                }
+            }
+
+            var response = await client.SendEmailAsync(msg);
+        }
+
+    }
+}
